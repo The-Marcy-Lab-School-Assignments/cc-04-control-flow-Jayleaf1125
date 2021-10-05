3b03df179d2c81ca8a283102077798e33ab3a051
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-10-05 14:34:18 +0000

    function 1 completed

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //1
 function fizzBuzz(){
   for(let i = 1; i <= 100; i++) {
-      if (i % 3 === 0) {
+      if (i % 3 === 0 && i % 5 === 0) {
+          console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
           console.log("Fizz");
       } else if (i % 5 === 0)  {
           console.log("Buzz");
@@ -11,13 +13,13 @@ function fizzBuzz(){
   }
 }
 
-//2
-// function fizzBuzz1(number){
+// 2
+function fizzBuzz1(number){
 //     const listStr
 //  for(let i = 1; i <= 15; i++) {
      
 //  }
-// }
+}
 
 
 // Call
