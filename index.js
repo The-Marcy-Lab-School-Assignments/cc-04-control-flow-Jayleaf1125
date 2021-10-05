f0d9b031647563a771a3b1286854c87b57c1f4d4
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-10-05 14:49:48 +0000

    Bonus Question Completed w/ Carmen

--- index.js
+++ index.js
@@ -15,12 +15,22 @@ function fizzBuzz(){
 
 // 2
 function fizzBuzz1(number){
-//     const listStr
-//  for(let i = 1; i <= 15; i++) {
-     
-//  }
+    let answer = [];
+    for(let i =1; i <= number; i++) {
+        if( i % 3 === 0 && i % 5 === 0) {
+            answer.push("fizzbuzz");
+        } else if (i % 5 === 0) {
+             answer.push("buzz");
+        } else if ( i % 3 === 0) {
+             answer.push("fizz");
+        } else {
+             answer.push(`${i}`);
+        }
+    }
+        return answer;
 }
 
 
 // Call
 fizzBuzz();
+console.log(fizzBuzz1(10));
